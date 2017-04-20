ee1b15c0afc81d1af8565a9e2d07d41a531a2d4b
Brilian T Nugraha <briliantnugraha@gmail.com>
2017-04-20 23:14:47 +0800

    Supervised and unsupervised dimentionality reduction
    In this part, i add some function to do supervised and unsupervised
    learning (though neither are working :D)
    
    For supervised learning (using PLSregression), it needs to have a
    complete data (no missing values)
    because the column ZipCity and some of the other columns have so many
    missing values, so  i can't run this step
    
    So, i try to use PearsonICA (unsupervised learning), which theoretically
    could handle missing values, but it requires numeric data
    
    So, what do you think guys?

--- 2. Ticket/project-2.R
+++ 2. Ticket/project-2.R
@@ -1,25 +1,57 @@
 library(dplyr)
 library(data.table)
-library(plsdepot)
 setwd("H:/Big data Analysis/Ticket")
+
+#read the csv data
 concert <- fread("concert.csv")
-# above <- concert[,like(TicketName, concert[-])]
+########################################################################
 
-table(above$SoldDate)
-#==========untile here===========================================
+########################## preprocess ##################################
+#filter based on ticketName
+#need a chinese language support, and a few mistic setting :D
+# i couldn't execute this one yet
+# above <- concert[,like(TicketName, "put the comparison here")]
 
-plot(ticket$SoldTime)
-library(randomForest)
+#group the concert by ticketname, solddate, and soldprice
+#then sum the data by that grouping, 
+#and pick only SoldDate, SoldPrice, TicketName, and total_per_date
+filter_concert <- group_by(concert, TicketName, SoldDate, SoldPrice) %>%
+                  summarise(total_per_date=n()) %>%
+                  select( SoldDate, SoldPrice, TicketName, total_per_date)
+########################################################################
 
+##################### dimensionality reduction ######################### 
+#in this part, we want to pick the important features
+#that might be useful to predict the salespeed of tickets
+#we try to use 2 methods:
 
-other <- ticket[-c(2,8)]
-head(other)
-fitS <- randomForest(as.factor(ticket$SoldDate,ticket$SoldPrice) ~  TicketCode,data=train,importance=TRUE,ntree=2000)
-Prediction <- predict(fit, test);Prediction #fit to predict test
-confusion_matrix<-(table(test$Survived,Prediction))
-print(confusionMatrix(confusion_matrix))
+#a. cluster the data w.r.t the classes (this one is using PLSregression)
+#b. cluster the data without knowing the classes (unsupervised learning)
 
-library(pls)
-plsr(other, ticket$SoldPrice)
+#################### a.PLSregression code ##############################
+#to simplify things, just put the classes on the right, and pick the classes
+#first, find the classes column location in the concert data, 
+#and reorganize the structure
 
-summary(ticket$TicketName)
+#---note, this gives error if there is blank/missing values---
+#so, what should we do about this guys? do we need to ignore it/anything else?
+library(plsdepot)
+#find the column location of SoldDate
+name_loc <- which(colnames(concert) == "SoldDate")
+
+#find the column location of SoldPrice
+name_loc2 <- which(colnames(concert) == "SoldPrice")
+#put all the class on the right
+concert_class_on_right <- concert[,c(1:2, 4:8, 10:20, 3, 9)]
+pls2<-plsreg2(concert_class_on_right[,1:18],
+        concert_class_on_right[,19:20, drop=FALSE], comps = 5)
+#######################################################################
+
+################## b.unsupervised learning code #######################
+library(MASS)
+library(PearsonICA)
+factor_concert <- as.factor( unlist(concert) )
+mca_result <- mca(factor_concert, nf = 5)
+
+# ica_result <- PearsonICA(X = concert, n.comp = 5, na.rm = TRUE)
+#######################################################################
